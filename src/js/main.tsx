3951980cac6bd09b7d3efb9b172b67d0af075a9f
Jonathan Sheely <jsheely@twentytech.net>
2017-01-17 14:44:22 -0500

    Removed scss

--- src/js/main.tsx
+++ src/js/main.tsx
@@ -1,8 +1,6 @@
 import * as React from 'react';
 import * as ReactDOM from 'react-dom';
 
-require('../style/main.scss');
-
 ReactDOM.render(
     <p className="red">Hello DNN Summit</p>,
     document.getElementById('appBody')
